27e6733e6bee75d9f724a6021f226f840665b508
Omar Shabaan <omar.plus.077@gmail.com>
2023-07-23 13:29:15 +0300

    re-indent the file to 2.

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entities/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entities/BaseEntity.java
@@ -1,26 +1,26 @@
 package com.food.ordering.system.domain.entities;
 
 public abstract class BaseEntity<ID> {
-    private ID id;
+  private ID id;
 
-    public ID getId() {
-        return id;
-    }
+  public ID getId() {
+    return id;
+  }
 
-    public void setId(ID id) {
-        this.id = id;
-    }
+  public void setId(ID id) {
+    this.id = id;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof BaseEntity<?> that)) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof BaseEntity<?> that)) return false;
 
-        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
-    }
+    return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+  }
 
-    @Override
-    public int hashCode() {
-        return getId() != null ? getId().hashCode() : 0;
-    }
+  @Override
+  public int hashCode() {
+    return getId() != null ? getId().hashCode() : 0;
+  }
 }
